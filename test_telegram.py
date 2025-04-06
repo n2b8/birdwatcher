c108b924ee08c3db4f86584f9875f4ce4d512c46
jake <mccaigjake@gmail.com>
2025-04-06 13:44:15 -0600

    fixed function name in telegram test

--- test_telegram.py
+++ test_telegram.py
@@ -1,5 +1,5 @@
 import os
-from send_telegram import send_telegram
+from send_telegram import send_telegram_message
 
 # Dummy values to test
 test_caption = "Test: A new House Finch visited your feeder!"
@@ -9,4 +9,4 @@ test_image_path = "visits/visit_20250405_170454.jpg"  # Use a real image path he
 if "TELEGRAM_BOT_TOKEN" not in os.environ or "TELEGRAM_CHAT_ID" not in os.environ:
     print("❌ Please set TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID in your environment.")
 else:
-    send_telegram(test_caption, test_image_path)
+    send_telegram_message(test_caption, test_image_path)
